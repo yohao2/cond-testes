525fcbb05e0109c71832d0640d103407c6d77f10
yohao2 <yohanhenriquelopes@gmail.com>
2026-07-26 17:56:40 -0300

    Add files via upload

--- pages/1_Formatador de Leituras.py
+++ pages/1_Formatador de Leituras.py
@@ -453,7 +453,7 @@ with direita:
         arquivos = listar_leituras(escolher_condominio)
 
         for aquivo in arquivos:
-            st.write(arquivo)
+            st.write('arquivo')
 
 
         nova_leitura = st.text_area('Salvar nova leitura:')
