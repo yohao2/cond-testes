2eb0b3124c69aac1d948a25958e47de476cca1ef
yohao2 <yohanhenriquelopes@gmail.com>
2026-07-26 17:08:35 -0300

    Add files via upload

--- pages/1_Formatador de Leituras.py
+++ pages/1_Formatador de Leituras.py
@@ -30,31 +30,32 @@ def salvar_no_dropbox(wb, nome_arquivo):
     time.sleep(1)
 
 def leitura_ibiza():
+    
+    st.title('RESIDENCIAL IBIZA')
+    st.write(condominios['Residencial Ibiza'])
+    st.write('---')
+
+    # checkbox
+    st.write('Escolha quais planilhas gerar:')
+    checkbox_agua = st.checkbox('Água')
+    checkbox_gas = st.checkbox('Gás')
+
+    if checkbox_agua:
+        st.subheader('Água')
+
+        # definir data gas 
+        data_agua = st.date_input('Data da leitura da água:', value= None, format='DD/MM/YYYY')
+        if data_agua:
+            dia_mes_agua = data_agua.strftime("%d/%m")
+
+        # definir valor da fatura
+        fatura = st.number_input('Valor da fatura (Com multa):')
+        # definir valor da multa
+        multa = st.number_input('Valor da multa (Deixe em branco caso não haja)')
+        # definir nome do arquivo
+        nome_arquivo_agua = st.text_input('Digite o nome que quer dar para o arquivo: ')
+
     with direita:
-
-        st.title('RESIDENCIAL IBIZA')
-        st.write(condominios['Residencial Ibiza'])
-        st.write('---')
-
-        # checkbox
-        st.write('Escolha quais planilhas gerar:')
-        checkbox_agua = st.checkbox('Água')
-        checkbox_gas = st.checkbox('Gás')
-
-        if checkbox_agua:
-            st.subheader('Água')
-
-            # definir data gas 
-            data_agua = st.date_input('Data da leitura da água:', value= None, format='DD/MM/YYYY')
-            if data_agua:
-                dia_mes_agua = data_agua.strftime("%d/%m")
-
-            # definir valor da fatura
-            fatura = st.number_input('Valor da fatura (Com multa):')
-            # definir valor da multa
-            multa = st.number_input('Valor da multa (Deixe em branco caso não haja)')
-            # definir nome do arquivo
-            nome_arquivo_agua = st.text_input('Digite o nome que quer dar para o arquivo: ')
 
         if checkbox_gas:
             st.subheader('Gás')
@@ -70,64 +71,64 @@ def leitura_ibiza():
             # definir nome do arquivo
             nome_arquivo_gas = st.text_input('Digite o nome que quer dar para o arquivo:')
 
-        st.write('---')
+    st.write('---')
 
-        # definir mes
-        mes_referencia = st.selectbox('Mês de referência', ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]).upper()
-
-
-        # definir leituras
-        leitura = st.text_area('Cole a leitura abaixo:')
-        leitura = leitura.splitlines()
-        botao_gerar = st.button('Gerar')
-
-        lista_agua = []
-        lista_gas = []
-
-        agua = 0
-        gas = 0
-
-        # formatar leituras
-        if botao_gerar:
-            for elemento in leitura:
-                if elemento == 'ÁGUA':
-                    agua = 1
-                if elemento == 'GÁS':
-                    gas = 1
-                    agua = 0
-
-                elemento = elemento[5:]
-                
-                if elemento != '':
-                    if agua == 1:
-                        elemento = elemento[:4] + ',' + elemento[4:]
-                        lista_agua.append(elemento.strip())
-                    if gas == 1:
-                        elemento = elemento[:5] + ',' + elemento[5:]
-                        lista_gas.append(elemento.strip())
+    # definir mes
+    mes_referencia = st.selectbox('Mês de referência', ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]).upper()
 
 
-            if not checkbox_agua and not checkbox_gas:
-                st.write('Selecione qual tipo de planilha gerar!')
-                st.stop()
+    # definir leituras
+    leitura = st.text_area('Cole a leitura abaixo:')
+    leitura = leitura.splitlines()
+    botao_gerar = st.button('Gerar')
 
-            if checkbox_agua:
-                if not data_agua or not fatura or not nome_arquivo_agua:
-                    st.write('Preencha todos os campos!')
-                    st.stop()
+    lista_agua = []
+    lista_gas = []
 
-            if checkbox_gas:
-                if not data_gas or not valor_bujao_unidade or not valor_bujao_total or not nome_arquivo_gas:
-                    st.write('Preencha todos os campos!')
-                    st.stop()
+    agua = 0
+    gas = 0
 
-            if not mes_referencia or not leitura:
+    # formatar leituras
+    if botao_gerar:
+        for elemento in leitura:
+            if elemento == 'ÁGUA':
+                agua = 1
+            if elemento == 'GÁS':
+                gas = 1
+                agua = 0
+
+            elemento = elemento[5:]
+            
+            if elemento != '':
+                if agua == 1:
+                    elemento = elemento[:4] + ',' + elemento[4:]
+                    lista_agua.append(elemento.strip())
+                if gas == 1:
+                    elemento = elemento[:5] + ',' + elemento[5:]
+                    lista_gas.append(elemento.strip())
+
+
+        if not checkbox_agua and not checkbox_gas:
+            st.write('Selecione qual tipo de planilha gerar!')
+            st.stop()
+
+        if checkbox_agua:
+            if not data_agua or not fatura or not nome_arquivo_agua:
                 st.write('Preencha todos os campos!')
                 st.stop()
 
-            st.write('Gerando planilhas...')
-            if checkbox_agua: planilha_agua_ibiza(dia_mes_agua, mes_referencia, fatura, multa, lista_agua, nome_arquivo_agua)
-            if checkbox_gas: planilha_gas_ibiza(dia_mes_gas, mes_referencia, valor_bujao_unidade, valor_bujao_total, lista_gas, nome_arquivo_gas)
+        if checkbox_gas:
+            if not data_gas or not valor_bujao_unidade or not valor_bujao_total or not nome_arquivo_gas:
+                st.write('Preencha todos os campos!')
+                st.stop()
+
+        if not mes_referencia or not leitura:
+            st.write('Preencha todos os campos!')
+            st.stop()
+
+        st.write('Gerando planilhas...')
+        if checkbox_agua: planilha_agua_ibiza(dia_mes_agua, mes_referencia, fatura, multa, lista_agua, nome_arquivo_agua)
+        if checkbox_gas: planilha_gas_ibiza(dia_mes_gas, mes_referencia, valor_bujao_unidade, valor_bujao_total, lista_gas, nome_arquivo_gas)
 
 def leitura_bali():
     st.title('RESIDENCIAL BALI')
